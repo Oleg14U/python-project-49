9237731ed99c1d2f4bdd37761d26c6d02e13fd8b
Oleg14U <hohleghek@yandex.ru>
2023-02-06 18:46:10 +0300

    6 fix

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,5 +2,5 @@ def wellcome_user():
     name = ''
     while name == '':
         print('May I have your name? ', end='')
-        name=input()
+        name = input()
     print(f'Hello, {name}!')
