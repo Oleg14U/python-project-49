f1f794c70d3fb58e59a1ebc3837df8afaf1f6994
Oleg14U <hohleghek@yandex.ru>
2023-02-22 16:26:23 +0300

    7 fix

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from brain_games.game_engine import play
-from brain_games.game import progression
+from brain_games.game.progression
 
 
 def main():
-    play(game)
+    play(brain_games.game.)
 
 
 if __name__ == '__main__':
