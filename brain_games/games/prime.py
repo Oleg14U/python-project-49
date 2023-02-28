e2086c5afb130e15d88f38846bf471cf5bb21c45
Oleg14U <hohleghek@yandex.ru>
2023-02-28 18:04:28 +0300

    8 fix

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,6 +1,7 @@
 from random import randint
 
-GAME_DESCRIPTION='Answer "yes" if given number is prime. Otherwise answer "no".'
+GAME_DESCRIPTION = X
+X = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 MIN_NUMBER = 1
 MAX_NUMBER = 10
 
